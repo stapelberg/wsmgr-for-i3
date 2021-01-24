a3792411c8284598cca0185a1cc175e54b4c46ab
Michael Stapelberg <michael@stapelberg.de>
2021-01-24 16:35:51 +0100

    add wsmgr-cwd wrapper which runs chdir if cwd symlink is present

---
+++ cmd/wsmgr-cwd/wsmgrcwd.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"syscall"
+
+	"go.i3wm.org/i3/v4"
+)
+
+var nameRe = regexp.MustCompile(`^\d+: (.*)`)
+
+func getWorkspaceName() string {
+	tree, err := i3.GetTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ws := tree.Root.FindFocused(func(n *i3.Node) bool { return n.Type == i3.WorkspaceNode })
+	if ws == nil {
+		log.Fatal("could not locate workspace")
+	}
+	name := ws.Name
+	if matches := nameRe.FindStringSubmatch(name); len(matches) > 1 {
+		name = matches[1]
+	}
+	return name
+}
+
+func chdir() error {
+	// get the current workspace’s name
+	workspaceName := getWorkspaceName()
+	log.Printf("workspace name = %q", workspaceName)
+
+	// check if ~/.config/zkj-wsmgr/<name>/cwd exists
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return err
+	}
+	target, err := os.Readlink(filepath.Join(configDir, "zkj-wsmgr", workspaceName, "cwd"))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	if err := os.Chdir(target); err != nil {
+		return err
+	}
+	return nil
+}
+
+func cwd() error {
+	if err := chdir(); err != nil {
+		log.Print(err)
+	}
+
+	// run the remaining command line args
+	args := os.Args[1:]
+	full, err := exec.LookPath(args[0])
+	if err != nil {
+		return err
+	}
+	return syscall.Exec(full, args, os.Environ())
+}
+
+func main() {
+	if err := cwd(); err != nil {
+		log.Fatal(err)
+	}
+}
